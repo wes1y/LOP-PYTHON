24563657cfa1d835fcb1d05dd8e68b57ce0cf602
wes1y <enhyped@gmail.com>
2023-01-22 18:56:37 -0300

    initial commit

---
+++ Uso do Len.py
@@ -0,0 +1,22 @@
+import os
+os.system('cls')
+
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    print(f'A primeira letra do seu nome é: {nome[0]}')
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'A última letra do seu nome é: {nome[-1]}')
+
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome não tem espaços')
+
+else:
+    print('Desculpe, você deixou campos vazios.')
+
+# commit1
